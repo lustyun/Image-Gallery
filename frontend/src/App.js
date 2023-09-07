13d62b3daa4a6940c0e4ded1a06d8d775aa7fa9c
Fardus Ahmed <fardus11@gmail.com>
2023-09-07 19:39:16 -0400

    added useeffect to make a get request from API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import "./App.css";
-import Login from "./components/Login";
+import { useEffect, useState } from "react";
+import Button from "./components/Button";
+import Grid from "./components/Grid";
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
 function App() {
+    const [photos, setPhotos] = useState([]);
+    const [updateUI, setUpdateUI] = useState("");
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:5000/api/get")
+            .then((res) => {
+                console.log(res.data);
+                setPhotos(res.data);
+            })
+            .catch((err) => console.log(err));
+    }, [updateUI]);
+
     return (
         <>
             <Navbar />;
-            
         </>
     );
 }
