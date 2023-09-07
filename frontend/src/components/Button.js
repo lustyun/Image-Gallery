3e68dfd880743bd7d38f46a70ac92942c203471b
Fardus Ahmed <fardus11@gmail.com>
2023-09-07 19:37:32 -0400

    added button render

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -3,19 +3,30 @@ import { AiFillPlusCircle } from "react-icons/ai";
 import axios from "axios";
 
 const Button = ({ setUpdateUI }) => {
-  const handleChange = (e) => {
-    e.preventDefault();
+    const handleChange = (e) => {
+        e.preventDefault();
 
-    const formData = new FormData();
-    formData.append("photo", e.target.files[0]);
+        const formData = new FormData();
+        formData.append("photo", e.target.files[0]);
 
-    axios
-      .post("http://localhost:5000/api/save", formData)
-      .then((res) => {
-        console.log(res.data);
-        setUpdateUI(res.data._id);
-      })
-      .catch((err) => console.log(err));
-  }
-  
-}
+        axios
+            .post("http://localhost:5000/api/save", formData)
+            .then((res) => {
+                console.log(res.data);
+                setUpdateUI(res.data._id);
+            })
+            .catch((err) => console.log(err));
+    };
+    return (
+        <label className="button" htmlFor="file_picker">
+            <AiFillPlusCircle />
+            <input
+                hidden
+                type="file"
+                name="file_picker"
+                id="file_picker"
+                onChange={(e) => handleChange(e)}
+            />
+        </label>
+    );
+};
